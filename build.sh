5fea94f4b403f36256740f2e5f1c8bd7c6c0a640
Meng Zhang <meng@pixelshift.ai>
2020-07-17 23:46:04 -0700

    Fix 32bit build

--- build.sh
+++ build.sh
@@ -3,28 +3,27 @@
 set -ex
 
 # Used in ./configure.
-export OPTIONS="--without-npm --with-intl=small-icu --shared"
-
 CMD=${1:-build_x86_64}
 TAG=${2:-14.2.0}
+
+export CONFIGURE_OPTIONS="--without-npm --with-intl=small-icu --shared"
 
 download_and_extract() {
   local FILENAME="v$TAG.tar.gz"
 
   curl -L https://github.com/nodejs/node/archive/${FILENAME} > $FILENAME
   tar zxvf "$FILENAME"
-  cp configure node-$TAG/
   cp android-configure node-$TAG/
 }
 
 
 build-android() {
   ./android-configure $ANDROID_NDK_HOME $ANDROID_ABI 23
-  make -j4
+   make -j4
 }
 
 build-linux-x86-64() {
-  ./configure --dest-cpu x86_64
+  ./configure --dest-cpu x86_64 $CONFIGURE_OPTIONS
   make -j4
 }
 
